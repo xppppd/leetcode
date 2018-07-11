342d94e2c4763ff3597c2299a36670a63030b08d
xiongpan <xppppd@gmail.com>
2018-07-11 17:56:53 +0800

    practice

---
+++ July/电话号码的字母组合.py
@@ -0,0 +1,43 @@
+'''
+给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+示例:
+
+输入："23"
+输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+说明:
+尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+'''
+import random
+
+
+class Solution:
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        dict = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
+        if digits == '':
+            return []
+        list = ['']
+        for i in digits:
+            list = [a + b for a in list for b in dict[i]]
+        return list
+
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s.letterCombinations('23')
+    print(r)
